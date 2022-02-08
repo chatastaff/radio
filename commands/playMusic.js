18a642511438989c1b518fb11540f2be7378fb54
chatastaff <83054570+chatastaff@users.noreply.github.com>
2022-02-08 22:04:50 +0300

    Update playMusic.js

--- commands/playMusic.js
+++ commands/playMusic.js
@@ -13,7 +13,7 @@ module.exports = (message, prefix) => {
         }
         
         voiceChannel.join().then(connection => {
-            connectionDispatcher = connection.play("https://www.rudaw.net/english/onair/radio/live");
+            connectionDispatcher = connection.play("https://liveonlineradio.net/player/?p=radio-nawa-kurdish");
             return message.channel.send("Playing");
         });
     } else if(message.content.startsWith(`${prefix} stop`)) {
