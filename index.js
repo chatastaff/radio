a7ab10d1eb5e6f0f32618de5219ab28895b296bb
chatastaff <83054570+chatastaff@users.noreply.github.com>
2022-02-08 21:23:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ fs.readdir("./events/", (err, files) => {
     });
 });
 
-client.login("OTE4NzI4ODM0MTY0MDY0MzE2.YbLe5w.g3KCwiZSZ-F8ZYHAFW8xn7A99cY");
+client.login("OTE4NzI4ODM0MTY0MDY0MzE2.YbLe5w.vuPDEpBpz-sz2ZtLQjng7XPUqDc");
